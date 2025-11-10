290a2ee7068a6efa962a95c28fc9c2114bc39d58
Jose Campos <j.camposs2701@gmail.com>
2025-11-10 15:40:57 -0500

    feat: Se realiza ajustes para que el usuario pueda realizar la respectiva descargar

--- customers/urls.py
+++ customers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
     # API para listar todos los clientes
     path('clientes/', views.ClienteListView.as_view(), name='cliente-list'),
+    path('clientes/download-csv/', views.ClienteDownloadCSVView.as_view(), name='cliente-download-csv'),
     
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Cliente
 from .serializers import (
     ClienteListSerializer
 )
+from django.http import HttpResponse
+import pandas as pd  # Importar pandas
 class ClienteListView(generics.ListAPIView):
     """
     API para listar todos los clientes activos con su
@@ -11,9 +14,39 @@ class ClienteListView(generics.ListAPIView):
     serializer_class = ClienteListSerializer
     
     def get_queryset(self):
-        # Optimizamos la consulta para evitar N+1 queries
-        # al acceder a 'documentos' y 'telefonos' en el serializer.
+
         return Cliente.objects.filter(activo=True).prefetch_related(
-            'documentos__tipo_documento', # Incluimos tipo_documento en el prefetch
+            'documentos__tipo_documento', 
             'telefonos'
         )
+
+class ClienteDownloadCSVView(APIView):
+    """
+    Vista para descargar un reporte de clientes en formato CSV
+    utilizando Pandas.
+    """
+    
+    def get(self, request, *args, **kwargs):
+        queryset = Cliente.objects.filter(activo=True).prefetch_related(
+            'documentos__tipo_documento', 
+            'telefonos'
+        )
+        serializer = ClienteListSerializer(queryset, many=True)
+        data = serializer.data
+        df = pd.DataFrame(data)
+        column_order = [
+            'tipo_documento',
+            'numero_documento',
+            'nombre', 
+            'apellido',
+            'correo',
+            'telefono'
+        ]
+        df = df.reindex(columns=[col for col in column_order if col in df.columns])
+
+        csv_data = df.to_csv(index=False, encoding='utf-8-sig')
+        
+        response = HttpResponse(csv_data, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="reporte_clientes.csv"'
+        
+        return response
